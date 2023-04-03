2f2ee07b8de91d0222d636a60509ca395fa7d880
abiola <abiola.arobieke@gmail.com>
2023-04-03 15:07:39 -0400

    Refactor index.js file

--- index.js
+++ index.js
@@ -1,90 +1,107 @@
-const bookData = JSON.parse(localStorage.getItem('bookData')) || [];
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const addBook = document.getElementById('add-book');
-const allBooks = document.getElementById('books');
-let allRemoveBtn;
-
-class AwesomeBook {
-  constructor(bookData, title, author, addBook, allBooks, allRemoveBtn) {
-    this.bookData = bookData;
-    this.title = title;
-    this.author = author;
-    this.addBook = addBook;
-    this.allBooks = allBooks;
-    this.allRemoveBtn = allRemoveBtn;
+class BookList {
+  constructor(bookListElement, addBookFormElement, titleInputElement, authorInput) {
+    this.bookListElement = bookListElement;
+    this.addBookFormElement = addBookFormElement;
+    this.titleInputElement = titleInputElement;
+    this.authorInput = authorInput;
+    this.books = [];
+    this.loadBooks();
+    this.updateBookList();
+    this.addBookFormElement.addEventListener('submit', this.add.bind(this));
   }
 
-  // Remove book from localStorage
-  remove() {
-    this.allRemoveBtn = document.querySelectorAll('.remove');
-    this.allRemoveBtn.forEach((removeBtn) => {
-      removeBtn.addEventListener('click', () => {
-        this.bookData.forEach((book, index) => {
-          if (index === parseInt(removeBtn.id, 10)) {
-            this.bookData.splice(index, 1);
-            localStorage.setItem('bookData', JSON.stringify(this.bookData));
-            this.loadBooks();
-          }
-        });
-      });
-    });
-  }
-
-  // Get all stored data and display on browser
   loadBooks() {
-    while (this.allBooks.hasChildNodes()) {
-      this.allBooks.removeChild(this.allBooks.firstChild);
+    const storedBooks = JSON.parse(localStorage.getItem('books'));
+    if (storedBooks) {
+      this.books = storedBooks.map((book) => ({
+        title: book.title,
+        author: book.author,
+      }));
     }
-
-    for (let i = 0; i < this.bookData.length; i += 1) {
-      const bookHTMLTemplate = `
-        <p>${this.bookData[i].title}</p>
-        <p>${this.bookData[i].author}</p>
-        <button id="${i}" class="remove" type="button">Remove</button>
-        <hr>`;
-      const eachBooks = document.createElement('div');
-      eachBooks.innerHTML = bookHTMLTemplate;
-      this.allBooks.appendChild(eachBooks);
-    }
-    this.remove();
   }
 
-  // Add a book to the bookData array
-  add() {
-    this.addBook.addEventListener('click', (event) => {
-      event.preventDefault();
-      if (this.author.value && this.title.value) {
-        const newBook = {
-          author: '',
-          title: '',
-        };
-        newBook.author = author.value;
-        newBook.title = title.value;
-        this.bookData.push(newBook);
-        localStorage.setItem('bookData', JSON.stringify(this.bookData));
-        this.author.value = '';
-        this.title.value = '';
-        this.loadBooks();
-      }
+  add(event) {
+    event.preventDefault();
+    const title = this.titleInputElement.value;
+    const author = this.authorInput.value;
+    const book = { title, author };
+    this.books.push(book);
+    this.titleInputElement.value = '';
+    this.authorInput.value = '';
+    this.updateBookList();
+  }
+
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title);
+    this.updateBookList();
+  }
+
+  updateBookList() {
+    this.bookListElement.innerHTML = '';
+    this.books.forEach((book) => {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `<strong>${book.title}</strong> by ${book.author}`;
+      const removeButton = document.createElement('button');
+      removeButton.id = 'remove-button';
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => {
+        this.removeBook(book.title);
+      });
+      const breakline = document.createElement('hr');
+      listItem.appendChild(removeButton);
+      listItem.appendChild(breakline);
+      this.bookListElement.appendChild(listItem);
     });
+    localStorage.setItem('books', JSON.stringify(this.books));
   }
 }
 
-const book = new AwesomeBook(
-  bookData,
-  title,
-  author,
-  addBook,
-  allBooks,
-  allRemoveBtn,
-);
+const bookListElement = document.getElementById('book-list');
+const addBookFormElement = document.getElementById('add-book-form');
+const titleInputElement = document.getElementById('title-input');
+const authorInput = document.getElementById('author-input');
 
-book.add();
-book.remove();
+const bookList = new BookList(bookListElement, addBookFormElement, titleInputElement, authorInput);
+bookList.loadBooks();
 
-//  Fetch data loading browser
-window.addEventListener('load', () => {
-  book.loadBooks();
-  book.remove();
-});
+function hideSectionsOnClick() {
+  const listSection = document.getElementById('all-books');
+  const addNewSection = document.getElementById('add-book');
+  const contactSection = document.getElementById('contact-us');
+
+  document.getElementById('list-link').addEventListener('click', () => {
+    listSection.style.display = 'block';
+    addNewSection.style.display = 'none';
+    contactSection.style.display = 'none';
+  });
+
+  document.getElementById('add-link').addEventListener('click', () => {
+    listSection.style.display = 'none';
+    addNewSection.style.display = 'block';
+    contactSection.style.display = 'none';
+  });
+
+  document.getElementById('contact-link').addEventListener('click', () => {
+    listSection.style.display = 'none';
+    addNewSection.style.display = 'none';
+    contactSection.style.display = 'flex';
+    // contactSection.classList.add('show');
+  });
+}
+
+hideSectionsOnClick();
+
+const options = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: true,
+};
+
+const now = new Date();
+const currentDate = now.toLocaleDateString('en-US', options);
+const DateTime = document.getElementById('date-time');
+DateTime.innerHTML = currentDate;
