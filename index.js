c053a02e83167d42ae7dff12516cdbb6c1ccf6bd
abiola <abiola.arobieke@gmail.com>
2023-04-04 11:32:20 -0400

    Update index.js file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { DateTime } from 'luxon.js';
 import BookList from './book.js';
 import hideSectionsOnClick from './spa.js';
+import getDateTime from './dateTime.js';
 
 // global variables
 const bookListElement = document.getElementById('book-list');
@@ -16,26 +16,6 @@ const bookList = new BookList(
   authorInput,
 );
 
-// Show current date and time
-const getDateTime = () => {
-  setTimeout(() => {
-    const todayDate = DateTime.now().setLocale('en-US').toLocaleString(DateTime.DATE_FULL);
-    const timeNow = DateTime.now()
-      .toLocaleString({
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        hourCycle: 'h12',
-      }).toUpperCase();
-    const dateTimeNow = `${todayDate} ${timeNow}`;
-    const dateTime = document.getElementById('date-time');
-    dateTime.innerHTML = dateTimeNow;
-    getDateTime();
-  }, 1000);
-};
-
-// Hide and show page section
 hideSectionsOnClick();
-
 bookList.loadBooks();
 getDateTime();
